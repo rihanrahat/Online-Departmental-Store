c83572892463c80354f7257f5397aa7962bcb167
Rihan Jamil Rahat <122556748+rihanrahat@users.noreply.github.com>
2025-04-16 03:18:27 +0600

    Online Departmental Store

---
+++ about-us.php
@@ -0,0 +1,61 @@
+<?php session_start();
+include_once('includes/config.php');
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shopping Portal | Category wise Shop </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+       <!--  <link href="css/bootstrap.min.css" rel="stylesheet" /> -->
+    </head>
+    <body>
+<?php include_once('includes/header.php');?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">About Us</h1>
+        
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+                Online shopping is changing the way people shop in Bangladesh. Why visit many stores to buy a new phone when you can get it online with just one click?
+
+Websites like Daraz, Pickaboo, and AjkerDeal have everything — phones, laptops, clothes, shoes, furniture, kitchen items, toys, and more.
+
+You can shop anytime — day or night — from the comfort of your home. Online shops never close!
+
+Plus, with big sales and discount offers all year, you can save money and buy more than you expected.
+
+So why wait? Shop smart, shop online!.
+
+                </div>
+            </div>
+
+ 
+</div>
+        </section>
+        <!-- Footer-->
+   <?php include_once('includes/footer.php'); ?>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,105 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Shopping Portal</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+    <div class="module-body m-t-20">
+        <p class="about-us"> Welcome to ABC Departmental Store, your one-stop online departmental store!
+
+We offer a wide range of products — from groceries, household essentials, personal care items, electronics, fashion, and more — all in one place. Our goal is to make shopping easy, fast, and convenient for everyone in Bangladesh.
+
+No more long queues or traffic jams. Just browse, click, and relax — we’ll deliver everything right to your doorstep.
+
+At ABC Departmental Store, we believe in quality, affordability, and great customer service. Whether you're shopping for daily needs or special items, we’re here to serve you better every day.
+
+Thank you for choosing us!</p>
+
+    </div>
+
+</div>          	</div>
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">opening time</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Saturday-Friday:</td><td class="pull-right">08.00 am To 10.00 pm</td></tr>
+				
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">information</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Dhaka, Bangladesh</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(+88) 01610174888<br>(+88) 01518988108</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="#">rahat15-4517@diu.edu.bd</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
